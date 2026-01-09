9170885d4a8c575068a3053e727cc0790af18ffc
Olawoore Damilola James <58667901+dammycute@users.noreply.github.com>
2026-01-09 20:12:21 +0100

    feat: implement the necessary configuration to host on vercel

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,15 +97,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('SUPABASE_DB_NAME'),
+#         'USER': config('SUPABASE_DB_USER'),
+#         'PASSWORD': config('SUPABASE_DB_PASSWORD'),
+#         'HOST': config('SUPABASE_DB_HOST'),
+#         'PORT': config('SUPABASE_DB_PORT', default=5432),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('SUPABASE_DB_NAME'),
-        'USER': config('SUPABASE_DB_USER'),
-        'PASSWORD': config('SUPABASE_DB_PASSWORD'),
-        'HOST': config('SUPABASE_DB_HOST'),
-        'PORT': config('SUPABASE_DB_PORT', default=5432),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
