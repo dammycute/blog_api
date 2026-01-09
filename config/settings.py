bfd81852f1a58a698b10b1b4f8324ce4e8b30c7f
Olawoore Damilola James <58667901+dammycute@users.noreply.github.com>
2026-01-09 20:00:19 +0100

    feat: implement the necessary configuration to host on vercel

--- config/settings.py
+++ config/settings.py
@@ -207,6 +207,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+DISABLE_COLLECTSTATIC = 1
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
